659b0930357390a4fa42fea2387fc39efd0a47be
Cafarli <80145532+Cafarli@users.noreply.github.com>
2024-06-20 15:00:36 +0400

    Create Make Class.py

---
+++ 7kyu/Object-oriented Programming/Make Class.py
@@ -0,0 +1,24 @@
+"""
+I don't like writing classes like this:
+
+class Animal:
+    def __init__(self, name, species, age, health, weight, color):
+        self.name = name
+        self.species = species
+        self.age = age
+        self.health = health
+        self.weight = weight
+        self.color = color
+Give me the power to create a similar class like this:
+
+Animal = make_class("name", "species", "age", "health", "weight", "color")
+"""
+
+
+def make_class(*args):
+    class Animal:
+        def __init__(self, *values):
+            for attr, value in zip(args, values):
+                setattr(self, attr, value)
+                
+    return Animal
