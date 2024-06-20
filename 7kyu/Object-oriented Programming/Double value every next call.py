d61905c697f9e76b9ada5bd6c486637ffaf466d0
Cafarli <80145532+Cafarli@users.noreply.github.com>
2024-06-20 14:50:04 +0400

    Create Double value every next call.py

---
+++ 7kyu/Object-oriented Programming/Double value every next call.py
@@ -0,0 +1,14 @@
+"""
+This kata is about static method that should return different values.
+
+On the first call it must be 1, on the second and others - it must be a double from previous value.
+
+Look tests for more examples, good luck :)
+"""
+
+class Class:
+    count = -1
+    @staticmethod
+    def get_number():
+        Class.count += 1
+        return 2**(Class.count)
