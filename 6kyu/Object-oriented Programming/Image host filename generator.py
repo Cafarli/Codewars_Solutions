a2c9f809b911c842dd7f351f277cc94d77e0922b
Cafarli <80145532+Cafarli@users.noreply.github.com>
2024-06-30 22:31:04 +0400

    Create Image host filename generator.py

---
+++ 6kyu/Object-oriented Programming/Image host filename generator.py
@@ -0,0 +1,22 @@
+"""
+You are developing an image hosting website.
+
+You have to create a function for generating random and unique image filenames.
+
+Create a function for generating a random 6 character string which will be used to access the photo URL.
+
+To make sure the name is not already in use, you are given access to an PhotoManager object.
+
+You can call it like so to make sure the name is unique
+
+// at this point, the website has only one photo, hosted on the 'ABCDEF' url
+photoManager.nameExists('ABCDEF'); // returns true
+photoManager.nameExists('BBCDEF'); // returns false
+Note: We consider two names with same letters but different cases to be unique.
+"""
+
+from random import choices
+def generateName():
+    letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 
+               'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']        
+    return ''.join(choices(letters, k=6))
